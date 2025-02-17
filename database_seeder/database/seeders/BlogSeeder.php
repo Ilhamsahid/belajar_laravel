599015b4af434b404536513c942a47614b1f349f
ilhamsahid <ilhamsahit988@gmail.com>
2025-02-17 19:36:03 +0700

    menambahkan truncate untuk menghindari duplicate

--- database_seeder/database/seeders/BlogSeeder.php
+++ database_seeder/database/seeders/BlogSeeder.php
@@ -13,9 +13,19 @@ class BlogSeeder extends Seeder
      */
     public function run(): void
     {
+        DB::table('blogs')->truncate();
+
         DB::table('blogs')->insert([
             'title' => "Blog 2",
             'description' => "Ini adalah description untuk blog 2 ",
+        ]);
+        DB::table('blogs')->insert([
+            'title' => "Blog 3",
+            'description' => "Ini adalah description untuk blog 3 ",
+        ]);
+        DB::table('blogs')->insert([
+            'title' => "Blog 4",
+            'description' => "Ini adalah description untuk blog 4 ",
         ]);
     }
 }
