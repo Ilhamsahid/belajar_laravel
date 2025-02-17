e357b824c494e147ec6b0783a820cc5d291b6a91
ilhamsahid <ilhamsahit988@gmail.com>
2025-02-17 19:34:25 +0700

    memasukkan data di column

--- database_seeder/database/seeders/BlogSeeder.php
+++ database_seeder/database/seeders/BlogSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class BlogSeeder extends Seeder
 {
@@ -12,6 +13,9 @@ class BlogSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('blogs')->insert([
+            'title' => "Blog 2",
+            'description' => "Ini adalah description untuk blog 2 ",
+        ]);
     }
 }
